fa0f7e58c72fbac1412ec74a63688cc326d1a3a0
Hasintha <73925316+Hasintha-git@users.noreply.github.com>
2021-07-05 17:29:15 +0530

    Article Blog site v1 Added to git

---
+++ models/article.js
@@ -0,0 +1,44 @@
+const mongoose=require('mongoose')
+const marked=require('marked')
+const slugify=require('slugify')
+// const createDomPurify = require('dompurify')
+// const { JSDOM } =require('jsdom')
+// const dompurify=createDomPurify(new JSDOM().window)
+
+const articleSchema = new mongoose.Schema({
+    title:{
+        type:String,
+        required:true
+    },
+    description: {
+        type:String
+    },
+    markDown: {
+        type:String,
+    },
+    createdAt: {
+        type:Date,
+        default:Date.now
+    },
+    slug:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    // sanitizedHtml: {
+    //     type:String,
+    //     required:true
+    // }
+})
+articleSchema.pre('validate',function(){
+    if(this.title){
+        this.slug=slugify(this.title,{lower:true,strict:true})
+    }
+
+    // if(this.markDown){
+    //     this.sanitizedHtml = dompurify.sanitize(marked(this.markDown))
+    // }
+
+
+})
+module.exports =mongoose.model('Article',articleSchema)
